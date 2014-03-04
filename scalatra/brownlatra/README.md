f97f010ac2e62398647588fd1af2a31eb6d326df
Devin Brown <devin.g.brown@gmail.com>
2014-03-04 08:58:57 -0700

    Include scalatra app.

---
+++ scalatra/brownlatra/README.md
@@ -0,0 +1,12 @@
+# Brownlatra #
+
+## Build & Run ##
+
+```sh
+$ cd Brownlatra
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ scalatra/brownlatra/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -0,0 +1,25 @@
+package com.example.app
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class MyScalatraServlet extends BrownlatraStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+  get("/devin") {
+    <html>
+      <body>
+        <h1>SCALVIN BROWNLATRA!</h1>
+      </body>
+    </html>
+  }
+
+}
